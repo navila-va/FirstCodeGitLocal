a4ff18636283b2e0e4461bf799a8198b8478ea22
malek <malek@DESKTOP-QVRMRD8.home>
2018-11-04 20:16:01 -0500

    new project for animal

---
+++ animal.py
@@ -0,0 +1,25 @@
+class Dog:
+
+    # Class Attribute
+    species = 'mammal'
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# Instantiate the Dog object
+philo = Dog("Philo", 5)
+mikey = Dog("Mikey", 6)
+
+# Access the instance attributes
+print("{} is {} and {} is {}.".format(
+    philo.name, philo.age, mikey.name, mikey.age))
+
+# Is Philo a mammal?
+if philo.species == "mammal":
+    print("{0} is a {1}!".format(philo.name, philo.species))
+#NOTE: Notice how we use dot notation to access attributes from each object.
+
+#Save this as dog_class.py, then run the program. You should see:
